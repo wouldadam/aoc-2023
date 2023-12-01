8d43faacdb47c71ac77210a322a14a9605ae3d13
Adam Wood <contact@adamwood.org>
2023-12-01 18:09:55 +0000

    Day 1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+
+int FindNum(string line)
+{
+    char? first = null;
+    char last = '0';
+
+    (string Name, char Val)[] numbers = [("one", '1'),
+        ("two" ,'2'), ("three", '3'), ("four", '4'), 
+        ("five", '5'), ("six", '6'), ("seven", '7'), 
+        ("eight", '8'), ("nine", '9')];
+
+    for(int idx = 0; idx < line.Length; idx++)
+    {
+        if ("0123456789".Contains(line[idx]))
+        {
+            last = line[idx];
+            if (first == null)
+            {
+                first = line[idx];
+            }
+        }
+        else
+        {
+            foreach (var num in numbers)
+            {
+                if (line.Substring(idx).StartsWith(num.Name))
+                {
+                    last = num.Val;
+                    if (first == null)
+                    {
+                        first = num.Val;
+                    }
+                }
+            }
+        }
+    }
+
+    return Int32.Parse($"{first}{last}");
+}
+
+// Load file
+Stream? stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Day1.input.txt");
+if (stream == null)
+{
+    Console.WriteLine("Failed to load input.");
+    return -1;
+}
+
+// Process each line
+int sum = 0;
+StreamReader reader = new(stream);
+while (!reader.EndOfStream)
+{
+    string? line = reader.ReadLine();
+    if (line != null)
+    {
+        int num = FindNum(line);
+        sum += num;
+    }
+}
+
+// Output the sum
+Console.WriteLine($"Sum of calibration values: {sum}");
+
+return 0;
